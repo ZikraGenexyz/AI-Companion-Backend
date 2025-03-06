866c073114ace4dccf494c309a31b526a71b68de
ZikraGenexyz <teqi@genexyz.world>
2025-03-06 14:48:27 +0700

    fix: error 500 vercel

--- ai_companion/wsgi.py
+++ ai_companion/wsgi.py
@@ -8,10 +8,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_companion.settings')
+
+if os.environ.get('ENV') == 'production':
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 application = get_wsgi_application()
 
