971f67a6afe267956702a63968613030bc0512e1
ZikraGenexyz <teqi@genexyz.world>
2025-03-09 21:16:59 +0700

    fix: database

--- companion_app/models.py
+++ companion_app/models.py
@@ -6,7 +6,6 @@ class User_Data(models.Model):
     username = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
     password = models.CharField(max_length=200)
-    
 
 class Chat_History(models.Model):
     key = models.ForeignKey(User_Data, related_name='chats', on_delete=models.CASCADE)
