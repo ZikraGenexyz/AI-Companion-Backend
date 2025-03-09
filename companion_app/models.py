e81bf1fc9fa0a41ef664c71f3858a748c200ecdf
ZikraGenexyz <teqi@genexyz.world>
2025-03-09 20:39:19 +0700

    fix: user database model

--- companion_app/models.py
+++ companion_app/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class User_Data(models.Model):
     id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
     username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
 
 class Chat_History(models.Model):
     key = models.ForeignKey(User_Data, related_name='chats', on_delete=models.CASCADE)
