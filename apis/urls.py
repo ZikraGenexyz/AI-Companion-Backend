75e3de93dd8245a20afa919e0ddd325c32dffc4e
ZikraGenexyz <teqi@genexyz.world>
2025-03-09 22:42:48 +0700

    feat: login api

--- apis/urls.py
+++ apis/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import ListUser, HistoryChat
+from .views import Login
 
 urlpatterns = [
     path('', ListUser.as_view()),
     path('chats', HistoryChat.as_view()),
+    path('login', Login(), name='login'),
 ]
--- apis/views.py
+++ apis/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from companion_app import models
 from .serializers import UserSerializer, ChatsSerializer
 from rest_framework import generics
+from rest_framework.response import Response, HTTP_200_OK, HTTP_400_BAD_REQUEST
+from django.contrib.auth import authenticate
 
 # Create your views here.
 class ListUser(generics.ListCreateAPIView):
@@ -11,3 +13,16 @@ class ListUser(generics.ListCreateAPIView):
 class HistoryChat(generics.ListCreateAPIView):
     queryset = models.Chat_History.objects.all()
     serializer_class = ChatsSerializer
+
+def Login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(email=email, password=password)
+
+        if user is not None:    
+            return Response({'message': 'Login successful'}, status=HTTP_200_OK)
+        else:
+            return Response({'message': 'Invalid credentials'}, status=HTTP_400_BAD_REQUEST)
+        
+    return Response({'message': 'Invalid request method'}, status=HTTP_400_BAD_REQUEST)
