aa0f1b286dec16b7a159fe6ab66e50ad154076ab
ZikraGenexyz <teqi@genexyz.world>
2025-03-11 11:28:50 +0700

    fix: chats database

--- companion_app/models.py
+++ companion_app/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-class User_Data(models.Model):
-    id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=200)
-    email = models.EmailField(max_length=200)
-    password = models.CharField(max_length=200)
-
 class Chat_History(models.Model):
-    key = models.ForeignKey(User_Data, related_name='chats', on_delete=models.CASCADE)
-    chat = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=1000)
+    isUser = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
