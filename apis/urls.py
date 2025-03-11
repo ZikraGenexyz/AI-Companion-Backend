c0dfcadeec1a86e4909830075f65ad70fd88946e
ZikraGenexyz <teqi@genexyz.world>
2025-03-11 22:18:04 +0700

    feat: AI Setup

--- apis/urls.py
+++ apis/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HistoryChat, Login, CreateAccount, AddChat
+from .views import HistoryChat, Login, CreateAccount, AddChat, GetChat
 
 urlpatterns = [
     path('/chats', HistoryChat.as_view()),
     path('/login', Login, name='login'),
     path('/create-account', CreateAccount, name='create-account'),
     path('/add-chat', AddChat, name='add-chat'),
+    path('/get-chat', GetChat, name='get-chat'),
 ]
