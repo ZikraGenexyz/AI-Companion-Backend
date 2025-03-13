1444013cae3f835efa740a2742906b494e8cd570
ZikraGenexyz <teqi@genexyz.world>
2025-03-13 15:53:00 +0700

    fix: auth method

--- companion_app/models.py
+++ companion_app/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
 class Chat_History(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_uid = models.CharField(max_length=100)
     text = models.TextField(max_length=1000)
     isUser = models.BooleanField(default=False)
