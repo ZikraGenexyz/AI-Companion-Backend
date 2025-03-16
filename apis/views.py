313e0415ae8aa98a6fd3413c3f24c96fe8a4acfb
ZikraGenexyz <teqi@genexyz.world>
2025-03-16 20:43:19 +0700

    fix: cartesia flexibility

--- apis/views.py
+++ apis/views.py
@@ -67,8 +67,7 @@ def GetChat(request):
 def AddChat(request):
     # Get the text from the request
     text = request.data['text']
-    # use_cartesia = request.data['useCartesia']
-    use_cartesia = True
+    use_cartesia = request.data['useCartesia']
 
     conversation_history = models.Chat_History.objects.filter(user_uid=request.data['user_uid'])
     conversation_history = [{'role': 'user' if item.isUser else 'assistant', 'content': item.text} for item in conversation_history]
