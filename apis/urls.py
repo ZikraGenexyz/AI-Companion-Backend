689a5317d6e20e8d622218f18b2f16549499b684
ZikraGenexyz <teqi@genexyz.world>
2025-03-24 16:35:31 +0700

    fix: google search

--- apis/urls.py
+++ apis/urls.py
@@ -3,11 +3,8 @@ from .views import HistoryChat, AddChat, GetChat, ResetChat, GoogleSearchApi
 
 urlpatterns = [
     path('/chats', HistoryChat.as_view()),
-    path('/create-account', CreateAccount, name='create-account'),
-    path('/check-user', CheckUser, name='check-user-availability'),
     path('/add-chat', AddChat, name='add-chat'),
     path('/get-chat', GetChat, name='get-chat'),
     path('/reset-chat', ResetChat, name='reset-chat'),
-    path('/get-deepgram-api', GetDeepgramAPI, name='get-deepgram-api'),
     path('/google-search', GoogleSearchApi, name='google-search-api'),
 ]
