8083f7d790f2de41be7f1e5a216f352675c4bf5d
ZikraGenexyz <teqi@genexyz.world>
2025-03-25 13:21:58 +0700

    fix: image generator

--- apis/views.py
+++ apis/views.py
@@ -90,7 +90,7 @@ def GenerateImage(request):
 
     if not useDummy:
         response = requests.post(
-            f"https://api.stability.ai/v2beta/stable-image/generate/core",
+            f"https://api.stability.ai/v2beta/stable-image/generate/sd3",
             headers={
                 "authorization": f"Bearer {os.getenv('STABILITY_API_KEY')}",
                 "accept": "image/*"
