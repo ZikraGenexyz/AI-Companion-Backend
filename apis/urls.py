fbf625807cb76b78a4d748c5f1570c542fea7b12
ZikraGenexyz <teqi@genexyz.world>
2025-04-01 21:21:31 +0700

    feat: friend list

--- apis/urls.py
+++ apis/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HistoryChat, AddChat, GetChat, ResetChat, GoogleSearchApi, GenerateImage
+from .views import HistoryChat, AddChat, GetChat, ResetChat, GoogleSearchApi, GenerateImage, Accept_Friend, Reject_Friend, Send_Friend_Request, Remove_Friend, Get_Friend_List
 
 urlpatterns = [
     path('/chats', HistoryChat.as_view()),
@@ -8,4 +8,9 @@ urlpatterns = [
     path('/reset-chat', ResetChat, name='reset-chat'),
     path('/google-search', GoogleSearchApi, name='google-search-api'),
     path('/generate-image', GenerateImage, name='generate-image'),
+    path('/accept-friend', Accept_Friend, name='accept-friend'),
+    path('/reject-friend', Reject_Friend, name='reject-friend'),
+    path('/send-friend-request', Send_Friend_Request, name='send-friend-request'),
+    path('/remove-friend', Remove_Friend, name='remove-friend'),
+    path('/get-friend-list', Get_Friend_List, name='get-friend-list'),
 ]
--- companion_app/models.py
+++ companion_app/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Chat_History(models.Model):
     id = models.AutoField(primary_key=True)
-    user_uid = models.CharField(max_length=100)
+    user_id = models.CharField(max_length=100)
     text = models.TextField(max_length=1000)
     isUser = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
+
+class Friends(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    friend_list = models.JSONField("Friends", default={})
+
+    
