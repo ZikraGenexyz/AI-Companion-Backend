546c5b4bee131a673648015166680917e9bf9829
ZikraGenexyz <teqi@genexyz.world>
2025-04-03 15:38:01 +0700

    fix: account users

--- apis/urls.py
+++ apis/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HistoryChat, AddChat, GetChat, ResetChat, GoogleSearchApi, GenerateImage, Accept_Friend, Reject_Friend, Send_Friend_Request, Remove_Friend, Get_Friend_List, User_Init, Search_User, Cancel_Friend_Request
+from .views import HistoryChat, AddChat, GetChat, ResetChat, GoogleSearchApi, GenerateImage, Accept_Friend, Reject_Friend, Send_Friend_Request, Remove_Friend, Get_Friend_List, User_Init, Search_User, Cancel_Friend_Request, Get_Account_Users, Account_Init
 
 urlpatterns = [
     path('/chats', HistoryChat.as_view()),
@@ -16,4 +16,6 @@ urlpatterns = [
     path('/user-init', User_Init, name='user-init'),
     path('/search-user', Search_User, name='search-user'),
     path('/cancel-friend-request', Cancel_Friend_Request, name='cancel-friend-request'),
+    path('/get-account-users', Get_Account_Users, name='get-account-users'),
+    path('/account-init', Account_Init, name='account-init'),
 ]
