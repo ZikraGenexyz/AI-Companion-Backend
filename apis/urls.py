7818c1de621d66f948d690da13be7fcbdf97756c
ZikraGenexyz <teqi@genexyz.world>
2025-04-09 11:18:02 +0700

    fix: unbind child

--- apis/urls.py
+++ apis/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('/remove-love-note', Remove_Love_Note, name='remove-love-note'),
     path('/edit-love-note', Edit_Love_Note, name='edit-love-note'),
     path('/get-children', Get_Children, name='get-children'),
-    path('/set-null-account', Set_NULL_Account, name='set-null-account'),
+    path('/unbind-children-account', Unbind_Children_Account, name='unbind-children-account'),
     path('/bind-children-account', Bind_Children_Account, name='bind-children-account'),
 ]
