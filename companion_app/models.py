f1ee45fd9024169f4203e0b03c245f2c836a0880
ZikraAknes <zikra.taqi@gmail.com>
2025-06-05 18:39:25 +0700

    fix: parents account model

--- companion_app/models.py
+++ companion_app/models.py
@@ -4,7 +4,11 @@ from django.db import models
 class Parents_Accounts(models.Model):
     id = models.AutoField(primary_key=True)
     account_id = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
+    date_of_birth = models.DateField(null=True)
+    phone_number = models.CharField(max_length=100, null=True)
+    relation = models.CharField(max_length=100, null=True)
     date_joined = models.DateTimeField(auto_now_add=True)
 
 class Children_Accounts(models.Model):
