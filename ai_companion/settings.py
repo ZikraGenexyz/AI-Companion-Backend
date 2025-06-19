1906147cee496601420e05cb69925fc2bd871600
ZikraAknes <zikra.taqi@gmail.com>
2025-06-19 17:29:01 +0700

    fix: .env override

--- ai_companion/settings.py
+++ ai_companion/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 from os import getenv
 
 # Load environment variables
-load_dotenv()
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
